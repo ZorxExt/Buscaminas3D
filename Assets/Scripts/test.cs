d4094d4b5ef175b55825921190cf7a8f6fc4ada4
agosav <91240071+agosav@users.noreply.github.com>
2022-08-23 00:31:59 -0300

    lol

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -12,7 +12,7 @@ public class Test : MonoBehaviour
     {
         
         Scripter.scripter.CreateTable(-5,4,-5,4,-5,4);
-        Scripter.scripter.CreateTable(15,30,15,30,15,30);
+        //Scripter.scripter.CreateTable(15,30,15,30,15,30);
         
     }
 
