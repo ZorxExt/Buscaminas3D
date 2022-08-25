5e726d1966f8f6c995a7b94bf8590667480e2cee
ZorxExt <franciscosabatecastillo@gmail.com>
2022-08-25 19:25:52 -0300

    Lost y Win tienen tema
    No me puse a buscar bugs, pero en principio funciona bien

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -18,4 +20,23 @@ public class UIManager : MonoBehaviour
         
         winUI.SetActive(switcher);
     }
+
+    public void ActivarDarkMode(bool switcher)
+    {
+        if (switcher)
+        {
+            lostUI.GetComponent<TextMeshProUGUI>().color = Color.gray;
+            lostUI.transform.Find("RestartButton").GetComponent<Image>().color = new Color(0.8f, 0.8f, 0.8f);
+            lostUI.transform.Find("RestartButton/RestartText").GetComponent<TextMeshProUGUI>().color = Color.black;
+
+        }
+        else
+        {
+            lostUI.GetComponent<TextMeshProUGUI>().color = Color.black;
+            lostUI.transform.Find("RestartButton").GetComponent<Image>().color = new Color(0.2f, 0.2f, 0.2f);
+            lostUI.transform.Find("RestartButton/RestartText").GetComponent<TextMeshProUGUI>().color = Color.gray;
+        }
+    }
+    
+    
 }
