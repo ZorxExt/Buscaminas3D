0f9dd707275710475fafa2653d7ab766cf0d8e9c
ZorxExt <franciscosabatecastillo@gmail.com>
2022-08-25 20:33:01 -0300

    el pepe
    cosas de colores de fondo

--- Assets/Scripts/GestorEscenas.cs
+++ Assets/Scripts/GestorEscenas.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class GestorEscenas : MonoBehaviour
 {
+    private void Start()
+    {
+        RenderSettings.skybox.SetColor("_Tint", Color.black);
+    }
 
     public void CambiarEscena(string escena)
     {
